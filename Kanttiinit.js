985ed4cbd4d2ba3d1fd8489e72745078ff388917
Max Huttunen <max.huttunen@gmail.com>
2015-12-17 14:16:54 +0200

    add icons to tabs

--- Kanttiinit.js
+++ Kanttiinit.js
@@ -5,6 +5,7 @@ import Material from 'react-native-material-kit';
 import Menu from './views/Menu';
 import Favourites from './views/Favourites';
 import Restaurants from './views/Restaurants';
+import Icon from 'react-native-vector-icons/Ionicons';
 const {
    AppRegistry,
    StyleSheet,
@@ -22,11 +23,12 @@ const {
 
 class TabButton extends React.Component {
    render() {
-      const {data, changeScene, parent, current} = this.props;
+      const {data, changeScene, parent, current, icon} = this.props;
       return (
          <MKButton
             backgroundColor={current ? MKColor.Teal : MKColor.Grey}
             onPress={() => changeScene(data)} style={styles.tabButton}>
+            <Icon name={icon} size={24} color={MKColor.Silver} />
             <Text style={{fontSize: 14, color: MKColor.Silver}}>{data.title}</Text>
          </MKButton>
       );
@@ -38,9 +40,9 @@ class Kanttiinit extends React.Component {
       super();
       this.state = {
          views: [
-            { title: 'MENU', component: React.createElement(Menu) },
-            { title: 'SUOSIKIT', component: React.createElement(Favourites) },
-            { title: 'RAVINTOLAT', component: React.createElement(Restaurants) }
+            { title: 'MENU', icon: 'android-restaurant', component: React.createElement(Menu) },
+            { title: 'SUOSIKIT', icon: 'android-favorite', component: React.createElement(Favourites) },
+            { title: 'RAVINTOLAT', icon: 'ios-list', component: React.createElement(Restaurants) }
          ],
          currentView: 'Menu'
       };
@@ -69,6 +71,7 @@ class Kanttiinit extends React.Component {
                      ref={'tabButton' + v.title}
                      current={this.state.currentView === v.title}
                      changeScene={this.changeScene.bind(this)}
+                     icon={v.icon}
                      key={v.title}
                      data={v} />
                )}

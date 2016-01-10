1bb19c05ac911f627212575370ba145a85616857
Max Huttunen <max.huttunen@gmail.com>
2016-01-10 16:47:42 +0200

    fix bug, worsen perf :(

--- views/Menu/Day.js
+++ views/Menu/Day.js
@@ -35,16 +35,10 @@ export default class Day extends Component {
    update(props) {
       const sorted = Service.formatRestaurants(props.restaurants, props.date, props.favorites);
       this.setState({
-         restaurants: sorted,
-         order: sorted.map(r => r.id).join(',')
+         restaurants: sorted
       });
    }
    shouldComponentUpdate(nextProps) {
-      if (nextProps.restaurants && this.state.order) {
-         const newSort = Service.formatRestaurants(nextProps.restaurants, nextProps.date, nextProps.favorites).map(r => r.id);
-         return newSort.join(',') !== this.state.order;
-      }
-
       return true;
    }
    render() {

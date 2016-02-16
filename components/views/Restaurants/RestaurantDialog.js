be0def83ef51754b1539c4973b6576e227989cc3
Max Huttunen <max.huttunen@gmail.com>
2016-02-16 14:34:13 +0200

    show own location and restaurant on map simultaneously

--- components/views/Restaurants/RestaurantDialog.js
+++ components/views/Restaurants/RestaurantDialog.js
@@ -2,34 +2,48 @@
 
 import React from 'react-native';
 import MapView from 'react-native-maps';
+import geolib from 'geolib';
+
+import {connect} from 'react-redux';
 
 const {
    View,
    Text
 } = React;
 
-export default class RestaurantDialog extends React.Component {
+class RestaurantDialog extends React.Component {
    render() {
-      const {restaurant} = this.props;
-      const coords = {
-         latitude: restaurant.latitude,
-         longitude: restaurant.longitude
-      };
+      const {restaurant, location} = this.props;
+      const center = geolib.getCenter([restaurant, location]);
       return (
          <View>
-            <Text style={{fontSize: 20}}>{restaurant.name}</Text>
+            <Text style={{fontSize: 22}}>{restaurant.name}</Text>
             <MapView
                style={{height: 300}}
                rotateEnabled={false}
                initialRegion={{
-                  ...coords,
-                  latitudeDelta: 0.02,
-                  longitudeDelta: 0.02
+                  latitude: Number(center.latitude),
+                  longitude: Number(center.longitude),
+                  latitudeDelta: 2.3 * Math.abs(center.latitude - restaurant.latitude),
+                  longitudeDelta: 2.3 * Math.abs(center.longitude - restaurant.longitude)
                }}>
                <MapView.Marker
-                  coordinate={coords}/>
+                  coordinate={{
+                     latitude: restaurant.latitude,
+                     longitude: restaurant.longitude
+                  }}/>
+               <MapView.Circle
+                  radius={10}
+                  strokeWidth={10}
+                  center={location} />
             </MapView>
          </View>
       );
    }
 }
+
+export default connect(
+   state => ({
+      location: state.location
+   })
+)(RestaurantDialog);

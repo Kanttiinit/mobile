07eda6a64f7b2d4e35d560b057dc895b1d2af1f8
Max Huttunen <max.huttunen@gmail.com>
2016-04-05 16:38:44 +0300

    fix codepush stuff

--- android/app/src/main/java/com/kanttiinit/MainActivity.java
+++ android/app/src/main/java/com/kanttiinit/MainActivity.java
@@ -15,6 +15,11 @@ import java.util.List;
 
 public class MainActivity extends ReactActivity {
 
+   @Override
+   protected String getJSBundleFile() {
+      return CodePush.getBundleUrl();
+   }
+
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
@@ -41,7 +46,7 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
-            new CodePush(),
+        new CodePush("DCRdt9kX4Pi3zVZfF8Vk_haPpNrX4y8NxonCg", this, BuildConfig.DEBUG),
         new AirPackage(),
         new LinearGradientPackage(),
         new VectorIconsPackage()
--- ios/kanttiinit/AppDelegate.m
+++ ios/kanttiinit/AppDelegate.m
@@ -11,6 +11,8 @@
 
 #import "RCTRootView.h"
 
+#import "CodePush.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -31,7 +33,13 @@
    * on the same Wi-Fi network.
    */
 
-  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+   NSURL *jsCodeLocation;
+
+    #ifdef DEBUG
+        jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+    #else
+        jsCodeLocation = [CodePush bundleURL];
+    #endif
 
   /**
    * OPTION 2

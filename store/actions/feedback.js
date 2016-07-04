7a78ec04bfce4822b8e0238e8f862114256ae82e
Max Huttunen <max.huttunen@gmail.com>
2016-07-04 18:44:02 +0300

    feedback is working

--- store/actions/feedback.js
+++ store/actions/feedback.js
@@ -4,10 +4,12 @@ export const SET_MESSAGE = 'SET_MESSAGE';
 export function send(type, message) {
    return {
       type: SEND_FEEDBACK,
-      payload: fetch('https://api.kanttiinit.fi/send-message', {
+      payload: fetch('https://bot.kanttiinit.fi/feedback', {
          method: 'post',
          headers: {'Content-Type': 'application/json'},
-         body: JSON.stringify({type, message})
+         body: JSON.stringify({
+            message: `New feedback from app: "${type}":\n"${message}"`
+         })
       })
       .then(r => r.json())
    };

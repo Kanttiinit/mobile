b74b7e137ec72babbb405fac096a2ad37dc2d050
Max Huttunen <max.huttunen@gmail.com>
2016-07-28 19:09:41 +0300

    fix android

--- android/app/src/main/java/com/kanttiinit/MainApplication.java
+++ android/app/src/main/java/com/kanttiinit/MainApplication.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import com.facebook.react.ReactApplication;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -28,8 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new MapsPackage(),
-            new VectorIconsPackage(),
-            new CodePush(this.getResources().getString(R.strings.reactNativeCodePush_androidDeploymentKey), this, BuildConfig.DEBUG)
+            new VectorIconsPackage()
       );
     }
   };
